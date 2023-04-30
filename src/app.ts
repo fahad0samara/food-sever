a388e85052c1615fb8e66845b4af844f60356c7b
fahad <90055525+fahad0samara@users.noreply.github.com>
2023-04-30 19:20:41 +0300

    frist commit

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const app = express();
+// Connect to MongoDB
+mongoose
+  .connect(
+    "mongodb+srv://fahad:fahad@cluster.zwkjqce.mongodb.net/back-end-helthe",
+    {useNewUrlParser: true, useUnifiedTopology: true}
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((error: any) => console.error(error));
+
+app.use(cors());
+app.use(morgan("common"));
+app.use(helmet());
+app.use(express.json());
+
+app.get("/", (req: any, res: {json: (arg0: {message: string}) => void}) => {
+  res.json({
+    message: "Hello World",
+  });
+});
+
+const port = process.env.PORT || 1337;
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
