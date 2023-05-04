57bac8eb1b8fbd52baafa8b78f0b258a27ab7af2
fahad <90055525+fahad0samara@users.noreply.github.com>
2023-05-04 14:41:48 +0300

    add multer and Azure storg to save the image form the server

--- src/router/menu.ts
+++ src/router/menu.ts
@@ -3,71 +3,136 @@ import {Category, Menu} from "../Model/Category";
 import {menuValidation, categoryValidation} from "../validate/schemas";
 const router = express.Router();
 
-// // Get all menu items
-// router.get('/menu', async (req: any, res:any) => {
-//   try {
-//     const menuItems = await Menu.find().populate('category');
-//     res.json(menuItems);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Error retrieving menu items.');
+// const {BlobServiceClient} = require("@azure/storage-blob");
+// const uuid = require("uuid");
+// const multer = require("multer");
+
+
+
+
+// // configure Azure Blob Storage
+// const blobServiceClient = BlobServiceClient.fromConnectionString(
+//   "DefaultEndpointsProtocol=https;AccountName=fahadtorg;AccountKey=fEMmu2yIHwvFS6fXIIQ0Wc+aF8jcMD+BVsIOVk1ngzunUei15auf3xxiVcNej1dA3jBDf2PxMUpu+AStXiZhdg==;EndpointSuffix=core.windows.net"
+// );
+// const containerName = "menu-items"; // set the container name
+// const containerClient = blobServiceClient.getContainerClient(containerName);
+
+// // create the container if it does not exist
+// const createContainerIfNotExists = async () => {
+//   const containerExists = await containerClient.exists();
+//   if (!containerExists) {
+//     await containerClient.create();
+//     console.log(`The container "${containerName}" has been created`);
 //   }
-// });
+// };
+// createContainerIfNotExists();
 
-// // Get menu items by category
-// router.get("/menu/:categoryId", async (req, res) => {
-//   const {categoryId} = req.params;
+// // configure Multer to use Azure Blob Storage as the storage engine
+// const storage = multer.memoryStorage();
+// const upload = multer({storage: storage});
 
+// // define a route to handle file uploads
+// router.post("/menu", upload.single("image"), async (req:any, res) => {
 //   try {
-//     const category = await Category.findById(categoryId);
-//     if (!category) {
-//       return res.status(404).send("Category not found");
-//     }
-//     const menuItems = await Menu.find({category: categoryId}).populate(
-//       "category"
+//     // get the file info from the request
+//     const file = req.file;
+
+//     // generate a unique filename for the file
+//     const filename = `${uuid.v4()}.${file.originalname.split(".").pop()}`;
+
+//     // create a new block blob with the generated filename
+//     const blockBlobClient = containerClient.getBlockBlobClient(filename);
+
+//     // upload the file to Azure Blob Storage
+//     await blockBlobClient.upload(file.buffer, file.buffer.length);
+
+//     // create a new menu item with the file URL returned by Azure Blob Storage
+//     const newItem = {
+//       name: req.body.name,
+//       description: req.body.description,
+//       category: req.body.category,
+//       price: req.body.price,
+//       image: blockBlobClient.url,
+//     };
+
+//     // send the saved item as the response
+//     res.json(newItem);
+//     console.log(
+//       `File uploaded successfully. ${blockBlobClient.url} ${req.body.name}`
+
 //     );
-//     res.json(menuItems);
+    
 //   } catch (error) {
 //     console.error(error);
-//     res.status(500).send("Error retrieving menu items.");
+//     res.status(500).json({error: "Server error"});
 //   }
 // });
+// const {BlobServiceClient} = require("@azure/storage-blob");
+// const multer = require("multer");
+// const storage = multer.memoryStorage();
+// const upload = multer({storage});
 
-// // Add a new menu item
-// router.post("/menu", async (req:any , res:any) => {
-//   const {name, description, category, price, image} = req.body;
+// const AZURE_STORAGE_CONNECTION_STRING =
+//   "DefaultEndpointsProtocol=https;AccountName=fahadtorg;AccountKey=fEMmu2yIHwvFS6fXIIQ0Wc+aF8jcMD+BVsIOVk1ngzunUei15auf3xxiVcNej1dA3jBDf2PxMUpu+AStXiZhdg==;EndpointSuffix=core.windows.net";
+// const AZURE_STORAGE_CONTAINER_NAME = "fahadtorg";
 
+// const blobServiceClient = BlobServiceClient.fromConnectionString(
+//   AZURE_STORAGE_CONNECTION_STRING
+// );
+// const containerClient = blobServiceClient.getContainerClient(
+//   AZURE_STORAGE_CONTAINER_NAME
+// );
+
+// router.post("/menu", upload.single("image"), async (req: any, res) => {
+//   console.log(req.file);
 //   try {
-//     let menuItem = await Menu.create({
-//       name,
-//       description,
-//       category,
-//       price,
-//       image,
+//     const {error, value} = menuValidation(req.body);
+//     if (error) {
+//       return res.status(400).json({error: error.details[0].message});
+//     }
+
+//     // Check if the container exists
+//     const containerExists = await containerClient.exists();
+//     if (!containerExists) {
+//       // Create the container if it doesn't exist
+//       await containerClient.create();
+//       console.log(
+//         `Container "${AZURE_STORAGE_CONTAINER_NAME}" created successfully.`
+//       );
+//     }
+
+//     // Upload the file to the container
+//     const blobName = `${Date.now()}-${req.file.originalname}`;
+//     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+//     const uploadOptions = {
+//       blobHTTPHeaders: {
+//         blobContentType: req.file.mimetype,
+//       },
+//       metadata: {
+//         originalName: req.file.originalname,
+//       },
+//     };
+//     const uploadResponse = await blockBlobClient.upload(
+//       req.file.buffer,
+//       req.file.size,
+//       uploadOptions
+//     );
+
+//     console.log(
+//       `File "${blobName}" uploaded successfully. ETag: ${uploadResponse.etag}, Last Modified: ${uploadResponse.lastModified}`
+//     );
+
+//     // Save the image URL in the database
+//     const menuItem = new Menu({
+//       ...value,
+//       image: blockBlobClient.url,
 //     });
 
-//     // find the category document by name
-//     let categoryDoc = await Category.findOne({name: category});
-
-//     if (!categoryDoc) {
-//       // if category doesn't exist, create a new one
-//       categoryDoc = await Category.create({
-//         name: category,
-//         description: `${category} items`,
-//         items: [menuItem._id],
-//       });
-//     } else {
-//       // check if the category already has the menu item
-//       const itemExists = categoryDoc.items.find(
-//         (item) => item.toString() === menuItem._id.toString()
-//       );
-//       if (itemExists) {
-//         return res.status(400).send("Menu item already exists in this category.");
-//       }
-//       // add the menu item to the category
-//       categoryDoc.items.push(menuItem._id);
-//       await categoryDoc.save();
-//     }
+//     const createdDate = new Date();
+//     const thirtyDaysAgo = new Date();
+//     thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+//     menuItem.isNew = createdDate > thirtyDaysAgo;
+//     await menuItem.save();
 
 //     res.status(201).json(menuItem);
 //   } catch (error) {
@@ -75,138 +140,115 @@ const router = express.Router();
 //     res.status(500).json({
 //       message: "Error adding menu item.",
 //       error,
-//     })
-//   }
-// });
-
-// // Create category endpoint
-// router.post("/categories", async (req, res) => {
-//   try {
-//     const {name, description} = req.body;
-
-//     const existingCategory = await Category.findOne({name});
-//     if (existingCategory) {
-//       return res.status(409).json({message: "Category already exists"});
-//     }
-
-//     const newCategory = await Category.create({name, description});
-//     res.status(201).json(newCategory);
-//     console.log(newCategory);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({
-//       message: "Error creating category.",
 //     });
 //   }
 // });
 
-// // get the category
-// router.get("/categories", async (req, res) => {
-//   try {
-//     const categories = await Category.find().populate("items");
-//     res.json(categories);
-//     console.log(
-//       "🚀 ~ file: menu.ts ~ line 127 ~ router.get ~ categories",
-//       categories
-//     );
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send("Error retrieving categories.");
-//   }
-// });
+const cloudinary = require("cloudinary").v2;
+const {CloudinaryStorage} = require("multer-storage-cloudinary");
+const multer = require("multer");
 
-// Create a new menu item and a new category
-router.post("/menu", async (req, res) => {
-  try {
-    const {error, value} = menuValidation(req.body);
-    if (error) {
-      return res.status(400).json({error: error.details[0].message});
-    }
-
-    const menu = new Menu(value);
-    const savedMenu = await menu.save();
-
-    res.status(201).json({
-      message: "Menu item created successfully",
-      menu: savedMenu,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      message: "Error creating menu item.",
-      error: err,
-    });
-  }
+// configure Cloudinary
+cloudinary.config({
+  cloud_name: "dh5w04awz",
+  api_key: "154856233692976",
+  api_secret: "sD9lI3ztLqo62It9mEias2Cqock",
 });
 
+// configure Multer to use Cloudinary as the storage engine
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: "menu-items", // set the folder name
+  allowedFormats: ["jpg", "png", "jpeg"], // specify the allowed file formats
+  transformation: [{width: 500, height: 500, crop: "limit"}], // specify any image transformations
+});
 
+// create a Multer instance with the configured storage engine
+const upload = multer({storage: storage});
+
+// define a route to handle file uploads
+router.post("/menu", upload.single("image"), async (req:any, res:any) => {
+  try {
+    // get the file info from the request
+    const file = req.file;
+
+    // create a new menu item with the file URL returned by Cloudinary
+    const newItem = new Menu({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      image: file.path,
+    });
+
+    // save the new menu item to the database
+    const savedItem = await newItem.save();
+
+    // send the saved item as the response
+    res.json(savedItem);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error: "Server error"});
+  }
+});
 
 
 // Get all menu items
-router.post("/menu", async (req, res) => {
+router.get("/menu", async (req: any, res: any) => {
   try {
-    const menuItem = new Menu(req.body);
-    const createdDate = new Date();
-    const thirtyDaysAgo = new Date();
-    thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
-    menuItem.isNew = createdDate > thirtyDaysAgo; // set isNew field based on criteria
-    await menuItem.save();
-    res.json(menuItem);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error creating menu item.");
-  }
-});
-
-// Get menu items by category
-router.get('/menu/:categoryId', async (req: any, res: any) => {
-  const { categoryId } = req.params;
-  const categoryDoc = await Category.findById(categoryId);
-  if (!categoryDoc) {
-    return res.status(404).send('Category not found');
-  }
-
-  try {
-    const menuItems = await Menu.find({ category: categoryId }).populate('category');
+    const menuItems = await Menu.find().populate("category");
     res.json(menuItems);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error retrieving menu items.');
+    res.status(500).send("Error retrieving menu items.");
+  }
+});
+
+// Get all menu items
+
+// Get menu items by category
+router.get("/menu/:categoryId", async (req: any, res: any) => {
+  const {categoryId} = req.params;
+  const categoryDoc = await Category.findById(categoryId);
+  if (!categoryDoc) {
+    return res.status(404).send("Category not found");
   }
 
+  try {
+    const menuItems = await Menu.find({category: categoryId}).populate(
+      "category"
+    );
+    res.json(menuItems);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving menu items.");
+  }
 });
 
 // get the category
 router.get("/categories", async (req, res) => {
   try {
-    const categories = await Category.find()
+    const categories = await Category.find();
     res.json(categories);
-
-
-    
-
   } catch (error) {
-   
     res.status(500).send({
       message: "Error retrieving categories.",
-      error: error
+      error: error,
     });
   }
-
 });
 
 router.post("/categories", async (req, res) => {
   try {
-    const { error, value } = categoryValidation(req.body);
+    const {error, value} = categoryValidation(req.body);
     if (error) {
-      return res.status(400).json({ error: error.details[0].message });
+      return res.status(400).json({error: error.details[0].message});
     }
 
     // Check if the category already exists
-    const category = await Category.findOne({ _id: value.id });
+    const category = await Category.findOne({_id: value.id});
     if (category) {
-      return res.status(400).json({ message: "Category already exists" });
-
+      return res.status(400).json({message: "Category already exists"});
     }
 
     const newCategory = new Category(value);
@@ -216,11 +258,6 @@ router.post("/categories", async (req, res) => {
       message: "Category created successfully",
       category: savedCategory,
     });
-    
-
-
-
- 
   } catch (err) {
     console.error(err);
     res.status(500).json({
@@ -229,15 +266,6 @@ router.post("/categories", async (req, res) => {
     });
   }
 });
-
-
-
-
-
-
-
-
-
 
 // // Create default caegories
 //   const defaultCategories = [
@@ -254,9 +282,5 @@ router.post("/categories", async (req, res) => {
 // } catch (error) {
 //   console.error(error);
 // }
-
-
-
-
 
 export default router;
